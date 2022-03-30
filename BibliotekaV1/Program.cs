808cce091f11a332c00ad224fb5c0a9f329e5266
MJL <maciekjl1993@gmail.com>
2022-03-30 19:19:02 +0200

    version 1.5

--- BibliotekaV1/Program.cs
+++ BibliotekaV1/Program.cs
@@ -8,21 +8,26 @@ namespace BibliotekaV1
     class Program
     {
         static List<Book> listOfBooks = new List<Book>();
+        static List<Rent> listOfRentedBooks = new List<Rent>();
         static void Main()
         {
-            Console.WriteLine("Co chcesz zrobić?");
-            Console.WriteLine("1\tDodaj książkę");
-            Console.WriteLine("2\tWypożycz książkę");
-            Console.WriteLine("3\tWyświetl książki");
-            Console.WriteLine("4\tPodaj info o wypożyczeniu");
-            Console.WriteLine("5\tObczaj książkę");
-            int choose = int.Parse(Console.ReadLine());
-            Choose(choose);
-
+            int choose = 0;
+            while (choose < 6)
+            {
+                Console.WriteLine("Co chcesz zrobić?");
+                Console.WriteLine("1\tDodaj książkę");
+                Console.WriteLine("2\tWypożycz książkę");
+                Console.WriteLine("3\tWyświetl książki");
+                Console.WriteLine("4\tPodaj info o wypożyczeniu");
+                Console.WriteLine("5\tObczaj książkę");
+                Console.WriteLine("6\tWyjdź");
+                choose = int.Parse(Console.ReadLine());
+                Choose(choose);
+            }
         }
         static void Choose(int choose)
         {
-            if (choose > 4 || choose<1) return;
+            if (choose > 5 || choose<1) return;
             switch(choose)
             {
                 case 1:
@@ -57,18 +62,60 @@ namespace BibliotekaV1
             
             listOfBooks.Add(Book.AddNewBook(authorFirstName, authorLastName, title, year));
             
+            //FOR DEBUG!!!!
+            //Console.WriteLine(listOfBooks.Count);
+            
         }
-        static void ViewBook() { }
-        static void RentBook() { }
-        static void ViewInfo() { }
+        static void ViewBook() 
+        {
+            Console.Clear();
+            Console.WriteLine($"Lista książek zawiera {listOfBooks.Count}\nKtóry element chcesz wyśiwetlić?\n");
+            int number = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Tytuł\tAutor\t\tRok wydania");
+            Console.WriteLine($"{listOfBooks[number].Title}\t{listOfBooks[number].FirstName} {listOfBooks[number].LastName}\t{listOfBooks[number].Year}");
+            Console.ReadKey();    
+        }
+        static void RentBook() 
+        {
+            ViewListOfBooks();
+            Console.WriteLine("Którą książkę chcesz wypożyczyć? Podaj numer pozycji\n");
+            int number = int.Parse(Console.ReadLine());
+            number--;
+            if (number < 0 || number > listOfBooks.Count+1)
+            {
+                Console.Clear();
+                RentBook();
+            }
+            Console.WriteLine("Podaj imię wypożyczającego");
+            string ownerFirstName = Console.ReadLine();
+            Console.WriteLine("Podaj nazwisko wypożyczającego");
+            string ownerLastName = Console.ReadLine();
+            DateTime wasTaken = DateTime.Today;
+            DateTime mustGiveBack = wasTaken.AddDays(30);
+            listOfRentedBooks.Add(Rent.RentBook(ownerFirstName, ownerLastName, listOfBooks[number].Title, listOfBooks[number].Year, listOfBooks[number].FirstName, listOfBooks[number].LastName, wasTaken, mustGiveBack));
+            listOfBooks.RemoveAt(number);
+
+        }
+        static void ViewInfo() 
+        {
+            
+            Console.WriteLine($"L.p.\t Imię i nazwisko\tTytuł\tAutor\t\tRok\tData wypoż.\tData oddania\n");
+            foreach(Rent rent in listOfRentedBooks)
+            {
+                Console.WriteLine($"{listOfRentedBooks.IndexOf(rent)+1}\t{rent.OwnerFirstName} {rent.OwnerLastName}\t{rent.Title}\t{rent.FirstName} {rent.LastName}\t{rent.Year}\t{rent.WasTaken}\t{rent.MustGiveBack}");
+            }
+        }
         static void ViewListOfBooks() 
         {
             Console.Clear();
             foreach(Book book in listOfBooks)
             {
-                Console.WriteLine("Imię autora\tNazwisko autora\tTytuł\tRok");
-                Console.WriteLine($"{book.FirstName}\t{book.LastName}\t{book.Title}\t{book.Year}");
+                Console.WriteLine("L.p.\tImię autora\tNazwisko autora\tTytuł\tRok");
+                Console.WriteLine($"{listOfBooks.IndexOf(book)+1}\t{book.FirstName}\t{book.LastName}\t{book.Title}\t{book.Year}");
             }
+            //FOR DEBUG
+            Console.WriteLine(listOfBooks.Count);
         }
         
     }
